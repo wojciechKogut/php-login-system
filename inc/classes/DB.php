daeeb950f1b6336d115c8144785f3912ca88d4a1
wojciechKogut <wkgt33@gmail.com>
2018-04-19 08:41:47 +0200

    include folder contains database connection, config, functions.

---
+++ inc/classes/DB.php
@@ -0,0 +1,28 @@
+
+<?php
+
+require_once("inc/config.php");
+
+class DB {
+
+    protected static $con;
+
+    private function __construct() {
+        try {
+            self::$con = new PDO('mysql:host=localhost;charset=utf8mb4;dbname=login_system', USER, "");
+            self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$con->setAttribute(PDO::ATTR_PERSISTENT, false);
+        }catch(PDOException $e) {
+            die("Database connection failed: ". $e->getMessage());
+        }
+    }
+
+    public static function getConnection() {
+        /** if no connection, run it */
+        if(!self::$con) new DB();
+    }
+
+
+
+
+}
---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * if you dont have defined constant config this file not load
+     */
+    if(!__CONFIG__) exit("You don't have config file");
+
+    define("USER", "root");
+
+
+
+?>
