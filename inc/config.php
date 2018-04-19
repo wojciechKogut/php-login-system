f75cab04c87c04a319c205449ed972c486bcceb7
wojciechKogut <wkgt33@gmail.com>
2018-04-20 00:54:54 +0200

    define document root constant

--- inc/config.php
+++ inc/config.php
@@ -3,9 +3,12 @@
      * if you dont have defined constant config this file not load
      */
     if(!__CONFIG__) exit("You don't have config file");
+    
+    $root = realpath($_SERVER['DOCUMENT_ROOT']);
+    define("DOCUMENT_ROOT", $root);
 
     define("USER", "root");
-    define("ROOT", "/php-login-system");
+    define("ROOT_FOLDER", "/php-login-system");
 
     if(!isset($_SESSION)) {
         session_start();
