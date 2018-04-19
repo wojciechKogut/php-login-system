428dd3834b275b47b1700f553324fbd9e02d922e
wojciechKogut <wkgt33@gmail.com>
2018-04-20 00:52:23 +0200

    refactor user login and register code

--- ajax/login.php
+++ ajax/login.php
@@ -6,9 +6,8 @@ define("__CONFIG__", true);
 /* require config */
 require_once("../inc/config.php");
 require_once("../inc/classes/Filter.php");
+require_once("../inc/classes/User.php");
 
-require_once("../inc/classes/DB.php");
-$con = DB::getConnection();
 
 
 if($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -17,30 +16,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $password = $_POST['password'];
 
     if(!empty($email) && !empty($password)) {
-        header("Content-type: application/json");
-
-        $data = [];
-    
-        $data['email'] = $email;
-        
-    
-        /** check if user exist */
-        $findUser = $con->prepare("SELECT * from users WHERE email = :email LIMIT 1 ");
-        $findUser->bindParam(":email",$data['email']);
-        $findUser->execute();
-
-        $results = $findUser->fetch(PDO::FETCH_ASSOC);
-    
-        if(password_verify($password,$results['password'])) {
-            $userId = $results['user_id'];
-            $_SESSION['user_id'] = $userId;
-            $data['redirect'] = ROOT . "/dashboard.php";
-        }else {
-
-            $data["err"] = "Invalid credential. Try again";
-        }
-        
-        /** convert php value into json value */
+        header("Content-type: application/json"); 
+        $data = User::logUser($email,$password);
         echo json_encode($data);
     }
     
--- ajax/register.php
+++ ajax/register.php
@@ -5,9 +5,10 @@ define("__CONFIG__", true);
 /* require config */
 require_once("../inc/config.php");
 require_once("../inc/classes/Filter.php");
+require_once("../inc/classes/User.php");
 
-require_once("../inc/classes/DB.php");
-$con = DB::getConnection();
+// require_once("../inc/classes/DB.php");
+// $con = DB::getConnection();
 
 
 if($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -19,40 +20,13 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
     if(!empty($name) && !empty($username) && !empty($email) && !empty($password)) {
         header("Content-type: application/json");
-
-        $data = [];
     
-        $data['name'] = Filter::string($name);
-        $data['username'] = Filter::string($username);
-        $data['email'] = Filter::email($email);
-        $data['password'] = password_hash($password,PASSWORD_BCRYPT,["cost"=>12]);
-    
-        /** check if user exist */
-        $findUser = $con->prepare("SELECT * from users WHERE email = :email LIMIT 1 ");
-        $findUser->bindParam(":email",$data['email']);
-        $findUser->execute();
-    
-        if($findUser->rowCount() == 1){
-            $data['err'] = "You have an account";
-        }else {
-            
-            /** add user to database */
-            $addUser = $con->prepare("INSERT INTO users (email,password,username) VALUES(:email,:password,:username)");
-            $addUser->bindParam(":email",$data['email']);
-            $addUser->bindParam(":password",$data['password']);
-            $addUser->bindParam(":username",$data['username']);
-            $addUser->execute();
+        $name = Filter::string($name);
+        $username = Filter::string($username);
+        $email = Filter::email($email);
+        $password = password_hash($password,PASSWORD_BCRYPT,["cost"=>12]);
 
-            /** get last added user id */
-            $userId = (int) $addUser->lastInsertId();
-
-            /** set session with user id */
-            $_SESSION['user_id'] = $userId;
-
-            $data['redirect'] = ROOT . "/dashboard.php";
-        }
-        
-        /** convert php value into json value */
+        $data = User::registerUser($email,$password,$name,$username);
         echo json_encode($data);
     }
     
