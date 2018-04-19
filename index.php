a98a709dfbf888e36820caa1e79e634dc39e754f
wojciechKogut <wkgt33@gmail.com>
2018-04-19 13:21:32 +0200

    update content

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php 
+
+define("__CONFIG__", true);
+
+/* require config */
+require_once("inc/config.php");
+
+require_once("inc/classes/DB.php");
+DB::getConnection();
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,13 +41,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Lorem ipsum</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Registration</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -55,5 +68,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="assets/js/main.js"></script>
   </body>
 </html>
